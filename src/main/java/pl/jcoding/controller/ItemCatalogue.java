5d056505863b574c2e71750b7c2239b43ba912d1
Jakub <nartowiczjakub@gmail.com>
2019-09-18 21:29:19 +0200

    ...

--- src/main/java/pl/jcoding/controller/ItemCatalogue.java
+++ src/main/java/pl/jcoding/controller/ItemCatalogue.java
@@ -3,10 +3,15 @@ package pl.jcoding.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+import pl.jcoding.entity.Item;
 import pl.jcoding.entity.ItemCategory;
+import pl.jcoding.entity.ItemGallery;
 import pl.jcoding.model.ItemsResponse;
 import pl.jcoding.service.ItemService;
+
+import java.util.List;
 
 @CrossOrigin("*")
 @RestController
@@ -26,6 +31,25 @@ public class ItemCatalogue {
         return itemService.getCategoryByName(categoryName);
     }
 
+    @GetMapping("/category/children/{category-id}")
+    public List<ItemCategory> getCategoryChildren(@PathVariable("category-id") String categoryId) {
+
+        return null;
+    }
+
+    @PreAuthorize("hasAuthority('ADMIN')")
+    @PutMapping("/category")
+    public void saveCategory(ItemCategory itemCategory) {
+
+    }
+
+    @PreAuthorize("hasAuthority('ADMIN')")
+    @DeleteMapping("/category")
+    public void deleteCategory(ItemCategory itemCategory) {
+
+    }
+
+    @PreAuthorize("hasAuthority('ADMIN')")
     @GetMapping("/item/{category-id}")
     public ItemsResponse getItemsPage(@PathVariable("category-id") Long categoryId,
                                       @RequestAttribute(value = "query", required = false) String query, //Search field
@@ -34,11 +58,26 @@ public class ItemCatalogue {
 
 
 
+        return new ItemsResponse();
+    }
+
+    @PostMapping("/item")
+    public Item saveItem(Item item) {
+        return null;
+    }
+
+    @PostMapping("/item/gallery")
+    public ItemGallery createItemGallery() {
+        return null;
+    }
+
+    @PostMapping("/item/gallery/add-photo")
+    public ItemGallery addPhotoToGallery(ItemGallery itemGallery) {
         return null;
     }
 
 
 
 
-    
+
 }
